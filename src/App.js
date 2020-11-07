b4afc3d981c2362d70350053b812b202c8ea045b
Abukari Einus <40009807+blackdante101@users.noreply.github.com>
2020-11-07 17:37:35 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import dante from './gandi.png';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={dante} className="App-logo" alt="logo" />
+        <p>
+        " An eye for eye only ends up making the whole world blind.<br/>
+Where there is love there is life.<br/>
+In a gentle way, you can shake the world. - Mahatma Gandhi"
+
+        </p>
+        
+      </header>
+
+    </div>
+    
+  );
+}
+
+export default App;
+
